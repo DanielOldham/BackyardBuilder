6dc9f74b064d3087cd7b8e0a19b74c4ae98999c7
Daniel Oldham <danieloldham@mail.weber.edu>
2022-11-14 18:35:15 -0700

    started search list view

--- components/views.py
+++ components/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import *
 
 
 # Create your views here.
 def search(request):
-    return render(request, 'components/search.html', {})
+    context = {}
+
+    components = Component.objects.all()
+    context['components'] = components
+
+    return render(request, 'components/search.html', context)
